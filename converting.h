2cb2be72642f06d539183d2160638e7eb151e2eb
Hanane <benidder1hanane2@gmail.com>
2023-11-30 22:55:34 +0100

    comments in converting.h

--- converting.h
+++ converting.h
@@ -1,8 +1,15 @@
-// CONVERTING MODULE HEADER 
-#ifndef _CONVERTING_H_ 
-#define _CONVERTING_H_ 
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-void converting(void);
-#endif 
+// This is the header for a converting module
+
+// These lines make sure we don't include this header more than once
+#ifndef _CONVERTING_H_  // Checks if _CONVERTING_H_ is not defined
+#define _CONVERTING_H_  // Defines _CONVERTING_H_ if it's not already
+
+// Including standard libraries for input/output, string handling, and general utilities
+#include <stdio.h>   
+#include <string.h>  
+#include <stdlib.h>  
+
+// Declaring a function that will be defined somewhere else
+void converting(void);  // This tells our program there's a function named converting
+
+#endif // This ends the if-statement from the start
